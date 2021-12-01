452c24cff08b20ffdd8cff60236b4891a8b7bb4b
Niklas Schlögel <niklasschloegel@gmail.com>
2021-12-01 09:51:06 +0100

    final solution of day01

---
+++ day-01/part1/main.py
@@ -0,0 +1,12 @@
+increase_count = 0
+
+with open("../input.txt") as input_file:
+    lines = input_file.read().splitlines()
+    for i, l in enumerate(lines):
+        if i == 0:
+            continue
+        if int(l) > int(lines[i-1]):
+            increase_count += 1
+
+
+print("{} measurements are larger than the previous one".format(increase_count))
---
+++ day-01/part2/main.py
@@ -0,0 +1,13 @@
+with open("../input.txt") as input_file:
+    lines = input_file.read().splitlines()
+    sums = [sum(list(map(lambda x: int(x), lines[i:i+3])))
+            for i in range(len(lines)-2)]
+
+    increase_count = 0
+    for i, l in enumerate(sums):
+        if i == 0:
+            continue
+        if int(l) > int(sums[i-1]):
+            increase_count += 1
+
+    print("{} sums are larger than the previous ones".format(increase_count))
