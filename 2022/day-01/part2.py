96b7ee406c2aaa493c2378384ac3db41a06aa9b5
Niklas Schlögel <niklasschloegel@gmail.com>
2022-12-01 08:58:39 +0100

    completing 2022 day 1

---
+++ 2022/day-01/part2.py
@@ -0,0 +1,15 @@
+with open("input.txt") as input_file:
+    lines = input_file.read().splitlines()
+
+    elves = []
+    while "" in lines:
+        seperator_idx = lines.index("")
+        elve = sum([int(e) for e in lines[:seperator_idx]])
+        elves.append(elve)
+        lines = lines[seperator_idx+1:]
+    
+    if len(lines):
+        elve = sum([int(e) for e in lines])
+        elves.append(elve)
+    
+    print(sum(sorted(elves, reverse=True)[:3]))
