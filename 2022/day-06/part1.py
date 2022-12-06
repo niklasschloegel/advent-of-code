ed9ca3aa10f9a9d57b85a971591a593ed1924ff4
Niklas Schlögel <niklasschloegel@gmail.com>
2022-12-06 10:57:04 +0100

    completing day 06 of 2022

---
+++ 2022/day-06/part1.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser
+
+debug_enabled = False
+
+
+def debug(out: str):
+    if debug_enabled:
+        print(out)
+
+
+# ----------------------------- SOLUTION CODE -----------------------------
+
+
+def solve(data: str) -> str:
+    count = 0
+    for i, char in enumerate(data):
+        char_set = {char, data[i + 1], data[i + 2], data[i + 3]}
+        if len(char_set) == 4:
+            return f"{count+4}"
+        count += 1
+
+
+# --------------------------- END SOLUTION CODE ---------------------------
+
+
+def main():
+    parser = ArgumentParser(description="Solve AoC puzzles")
+    parser.add_argument("filename")
+    parser.add_argument("-d", "--debug", action="store_true", default=False)
+
+    args = vars(parser.parse_args())
+    if not args:
+        parser.print_help()
+    else:
+        file_name = args["filename"]
+        global debug_enabled
+        debug_enabled = args["debug"]
+
+        with open(file_name) as data:
+            solution = solve(data.read())
+            print(solution)
+
+
+if __name__ == "__main__":
+    main()
